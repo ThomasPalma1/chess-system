ea236d8b1b327366d5f679045d399ac519009cce
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2023-10-27 14:20:55 -0300

    feature: improving the instance of pieces on the board
    - refining the strategy for instantiating pieces within the board.

--- chess-system/src/main/java/chess/ChessMatch.java
+++ chess-system/src/main/java/chess/ChessMatch.java
@@ -1,7 +1,7 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
+import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -23,8 +23,14 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(0, 0));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 
 }
