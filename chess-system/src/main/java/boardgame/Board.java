7ee86ca00bfcc41f20582a8bcb9507f505b695a0
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2023-10-27 10:52:58 -0300

    feature: checking piece and position
     - adding methods that checks if the position and piece exists.

--- chess-system/src/main/java/boardgame/Board.java
+++ chess-system/src/main/java/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -38,6 +41,21 @@ public class Board {
     public void placePiece(Piece piece, Position position) {
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
-
     }
+
+    private boolean positionExists(int row, int column) {
+        return row >= 0 &&
+                row < rows &&
+                column >= 0 &&
+                column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        return piece(position) != null;
+    }
+
 }
