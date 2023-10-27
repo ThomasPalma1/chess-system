ec25bc14d7c7e51650d96239255f91d12d005013
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2023-10-27 10:16:56 -0300

    feature: starting chess game
    - it is now possible to define the position of the pieces that were created within the board.

--- chess-system/src/main/java/chess/ChessMatch.java
+++ chess-system/src/main/java/chess/ChessMatch.java
@@ -1,12 +1,15 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
@@ -18,6 +21,10 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void initialSetup() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(0, 0));
     }
 
 }
