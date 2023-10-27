62adb4dfba63d4e357dde895b3fba36dc43cd7f9
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2023-10-27 11:05:18 -0300

    feature: implementing defensive programming
    - integrating validations in relation to pieces and positions;
    - checking the presence of a piece in a specific position or the existence of the position within the board.

--- chess-system/src/main/java/boardgame/Board.java
+++ chess-system/src/main/java/boardgame/Board.java
@@ -18,36 +18,37 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
 
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
 
     public Piece piece(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardException("Position not on the board");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
+
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("There is already a piece on position " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
 
     private boolean positionExists(int row, int column) {
-        return row >= 0 &&
-                row < rows &&
-                column >= 0 &&
-                column < columns;
+        return row >= 0 && row < rows && column >= 0 && column < columns;
     }
 
     public boolean positionExists(Position position) {
@@ -55,6 +56,9 @@ public class Board {
     }
 
     public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
         return piece(position) != null;
     }
 
