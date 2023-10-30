aa871a990d28970653aecca575fea77a3ee75383
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2023-10-30 15:29:17 -0300

    update: updating exception

--- chess-system/src/main/java/exceptions/ChessException.java
+++ chess-system/src/main/java/exceptions/ChessException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-public class ChessException extends RuntimeException {
+public class ChessException extends BoardException {
 
     public ChessException(String msg) {
         super(msg);
